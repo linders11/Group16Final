83320b6a73b17c963c33a5069446867e4c0ce71a
linders11 <stevensonlinder@gmail.com>
2025-06-04 10:22:45 -0700

    swapped order
    swapped order of register stack pushes to have them displayed in proper order

--- debug_skeleton_code/src/asm/dump_registers.s
+++ debug_skeleton_code/src/asm/dump_registers.s
@@ -6,22 +6,24 @@ dump_registers:
 
 subq $(16*8), %rsp
 movq %rsp, %rdi
-pushq %rax
-pushq %rbx
-pushq %rcx
-pushq %rdx
-pushq %rsi
-pushq %rdi
-pushq %rbp
-pushq %rsp
-pushq %r8
-pushq %r9
-pushq %r10
-pushq %r11
-pushq %r12
-pushq %r13
-pushq %r14
+/*push all register values to the stack to print the values. must be reverse
+order due to the nature of stacks*/
 pushq %r15
+pushq %r14
+pushq %r13
+pushq %r12
+pushq %r11
+pushq %r10
+pushq %r9
+pushq %r8
+pushq %rsp
+pushq %rbp
+pushq %rdi
+pushq %rsi
+pushq %rdx
+pushq %rcx
+pushq %rbx
+pushq %rax
 call _debug_dump_registers
 addq $(16*8), %rsp
 ret
